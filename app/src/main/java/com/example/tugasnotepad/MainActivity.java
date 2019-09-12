10963e38a491f7a5928f448f9df630323a6da998
dianahudani <dianakisyono@gmail.com>
2019-09-12 10:49:17 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugasnotepad/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.tugasnotepad;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class MainActivity extends AppCompatActivity {
+
+    static ArrayList<String> notes = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId() == R.id.add_note){
+            Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+
+            startActivity(intent);
+
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView ListView = findViewById(R.id.ListView);
+
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.tugasnotepad", Context.MODE_PRIVATE);
+
+        HashSet<String> set = (HashSet) sharedPreferences.getStringSet("notes", null);
+
+        if(set == null){
+            notes.add("Example note");
+        } else{
+            notes = new ArrayList(set);
+        }
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+        ListView.setAdapter(arrayAdapter);
+
+        ListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+               Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+               intent.putExtra("noteId", i);
+               startActivity(intent);
+            }
+        });
+
+        ListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final int itemToDelete = i;
+                new AlertDialog.Builder(MainActivity.this)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("Yakin?")
+                    .setMessage("Apakah anda ingin menghapus note ini?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            notes.remove(itemToDelete);
+                            arrayAdapter.notifyDataSetChanged();
+
+                            SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.tugasnotepad", MODE_PRIVATE);
+
+                            HashSet<String> set = new HashSet(MainActivity.notes);
+
+                            sharedPreferences.edit().putStringSet("notes", set).apply();
+                        }
+                    }
+                    )
+                        .setNegativeButton("No", null)
+                        .show();
+                return true;
+            }
+        });
+
+    }
+}
